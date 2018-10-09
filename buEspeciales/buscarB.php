a5c4ae2f32e716f6ba5aa70277618ba37b616da9
fjos807 <fjos807@gmail.com>
2018-10-09 10:42:40 -0600

    Especial B funcionando al 100%

---
+++ buEspeciales/buscarB.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Laboratorio MongoDB</title>
+        <style>
+            table, th, td {
+                border: 1px solid black;
+            }
+        </style>
+    </head>
+    <body>
+        <h2>Laboratorio MongoDB</h2>
+        <h3>Resultados de la busqueda</h3>
+
+        <table>
+            <tr>
+                <th>Nombre</th>
+                <th>Genero</th>
+                <th>Nombre Director</th>
+                <th>Franquicia</th>
+                <th>Pais</th>
+                <th>Fehca estreno</th>
+                <th>Duracion</th>
+                <th>Productora</th>
+                <th>Actores</th>
+            </tr>
+        <?php
+
+        $franquicia = $_POST['franquicias'];
+
+        require '../vendor/autoload.php';
+        $client = new MongoDB\Client;
+
+        $base = $client ->labmongo;
+        $coleccion = $base->peliculas;
+
+        $data = $coleccion->find(
+            ['franquicia' => $franquicia]
+        );
+
+
+        foreach ( $data as $current )
+            echo "<tr><td>". $current['nombre'] . "</td><td>". $current['genero'] . "</td><td>". $current['nombredirector'] . "</td><td>". $current['franquicia'] . "</td><td>". $current['pais'] . "</td><td>". $current['anoestreno'] . "</td><td>". $current['duracion'] . "</td><td>". $current['productora'] . "</td><td>". $current['actores']. "</td></tr>";
+        ?>
+        </table>
+    </body>
+</html>
